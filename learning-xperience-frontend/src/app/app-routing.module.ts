f4d3972883459cbdb4ec9142e57095d3062572b0
Patricia Fernandez <patriciafernandezoficial@gmail.com>
2022-06-12 20:01:14 +0200

    added backend

--- learning-xperience-frontend/src/app/app-routing.module.ts
+++ learning-xperience-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskboardComponent } from './taskboard/taskboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo: "taskboard"},
+  {path: "taskboard", component: TaskboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- learning-xperience-frontend/src/app/app.module.ts
+++ learning-xperience-frontend/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+
+import { TaskboardModule } from './taskboard/taskboard.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+
+    TaskboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
